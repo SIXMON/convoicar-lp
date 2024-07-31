86e18de037c5061fe442c15c491be326bfccdd1d
hmillot <Huogdu76@gmail.com>
2024-07-31 14:06:29 +0200

    fix: wait for document load js

--- src/assets/js/typeform.js
+++ src/assets/js/typeform.js
@@ -1,2 +1,4 @@
-const { open, close, toggle, refresh } = window.tf.createPopup('NJfm2KNi')
-document.querySelector('#typeform_contact').onclick = toggle
+document.addEventListener("DOMContentLoaded", ()=>{
+    const { open, close, toggle, refresh } = window.tf.createPopup('NJfm2KNi')
+    document.querySelector('#typeform_contact').onclick = toggle
+});
